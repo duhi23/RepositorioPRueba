0942e904277b16c41f36dc5e513585aff893ec95
Diego Paul Huaraca S <diego.21.phs@hotmail.com>
2025-04-16 15:42:36 +0000

    Rprofile

--- Scripts/01_Intro.R
+++ Scripts/01_Intro.R
@@ -1,4 +1,24 @@
 # Primer script del curso
+# Configuraciones iniciales
+179/5378641
+options(scipen = 999)
+options(digits = 2)
+
+# Instalacion de paquetes
+install.packages("data.table")
+install.packages("tidyverse", dependencies = TRUE)
+
+
+# Rprofile
+if(!file.exists("~/.Rprofile")){
+  file.create("~/.Rprofile")
+} else {
+  file.edit("~/.Rprofile")
+}
+
+
+
+
 
 # Vectores
 c(2,5,8,12)
@@ -8,3 +28,7 @@ mean(c(2,5,8,12))
 x <- c(2,5,8,12)
 sum(x)
 mean(x)
+
+x %>% sum()
+
+
